29bbcecf25a02b92a081da1af7eb1836a0e8254e
Vlad Ifju <mlem@Vlads-MacBook-Air.local>
2024-10-15 00:23:23 +0300

    added Jarvis March implementation

---
+++ algorithms/jarvisMarch.go
@@ -0,0 +1,41 @@
+package algorithms
+
+import (
+	"grahamScan/geometry"
+)
+
+func JarvisMarch(points []geometry.Point) []geometry.Point {
+    // exit case
+	if len(points) < 3 {
+		return points 
+	}
+
+	var hull_list []geometry.Point
+	y_leftmost := 0
+
+	// find the leftmost point
+	for i := 1; i < len(points); i++ {
+		if points[i].X < points[y_leftmost].X {
+			y_leftmost = i
+		}
+	}
+
+	p := y_leftmost
+	for {
+		hull_list = append(hull_list, points[p])
+		q := (p + 1) % len(points) // next point in the list
+
+		// find the most counter-clockwise point relative to point p
+		for i := 0; i < len(points); i++ {
+			if orientation(points[p], points[i], points[q]) > 0 {
+				q = i
+			}
+		}
+
+		p = q // move to the next point
+		if p == y_leftmost { // we wrapped around to the starting point
+			break
+		}
+	}
+	return hull_list
+}
